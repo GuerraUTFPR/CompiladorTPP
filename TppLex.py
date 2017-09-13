1c908b93615ea2d88e97c924eca54f655e65170f
GuerraUTFPR <guerramatheus2@gmail.com>
2017-09-13 16:37:20 -0300

    entrega realizada

--- TppLex.py
+++ TppLex.py
@@ -4,6 +4,7 @@
 import ply.lex as lex
 import sys
 
+#hash de palavras reservadas
 reserved = {
   'se'        :'SE',
   'então'     :'ENTAO',
@@ -19,7 +20,7 @@ reserved = {
   'principal' :'PRINCIPAL',
 
 }
-
+#vetor de tokens concatenado com o hash de palavras reservadas
 tokens = [
   'ID',
   'VIRGULA',
@@ -51,10 +52,9 @@ tokens = [
 
 ] + list(reserved.values())
 
-
-# Regular expression rules for simple tokens
+# Expressão regular para tokens simples
 t_SOMA = r'\+'
-t_SUBR = r'-'
+t_SUBR = r'\-'
 t_VEZES = r'\*'
 t_DIVIDE = r'\/'
 t_IGUAL = r'\='
@@ -73,44 +73,46 @@ t_ELOGICO = r'\&\&'
 t_OULOGICO = r'\|\|'
 t_NEGACAO = r'\!'
 t_DIFERENTE = r'\!\='
-#t_COMENTARIO = r'\{\s*.*\s*\}'
-#t_COMENTARIO = r'\{[^}]*[^{]*\}'
 
+
+# Expressão regular para identificadores e palavras reservadas
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z_0-9à-ú]*'
-    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    t.type = reserved.get(t.value,'ID') #verifica se é palavra reservada
     return t
 
+# Expressão regular para comentários, com tratamento de contagem de linhas
 def t_COMENTARIO(t):
-  #r'\{\s*.*\s*\}'
   r'\{[\s]*[^}]*[\s]*[^{]*\}'
-  for x in xrange(0,len(t.value)):
+  for x in xrange(0,len(t.value)): #verificando a quantidade de quebra de linhas
     if t.value[x] == '\n':
       t.lexer.lineno += 1
   return t
 
+# Expressão regular para identificar numeros flutuante, considerando o ponto (ex: 2. é flutuante)
 def t_FLUTUANTE(t):
     r'[0-9]+[\.][0-9]*'
     t.value = float(t.value)   
     return t  
 
+#exŕessão regular para identificar números inteiros
 def t_INTEIRO(t):
     r'[0-9]+'
     t.value = int(t.value)   
     return t
 
-# Define a rule so we can track line numbers
+# regra para contagem de linhas
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+# caracteres ignorados
+t_ignore  = '\t '
 
-# Error handling rule
+# Mostrar erro
 def t_error(t):
-    print("Caractére ilegal '%s'" % t.value[0])
+    #print "Caractére ilegal" + t.value[0]
     t.lexer.skip(1)
 
 
@@ -119,6 +121,8 @@ def t_error(t):
 def main():  
   lexer = lex.lex()
   f = open(sys.argv[1], 'r')
+
+  arq = open(sys.argv[1]+"_tokens.txt", 'w')
 
   data = f.read()
   
@@ -134,8 +138,11 @@ def main():
       valor = tok.value
       linha = tok.lineno
       s = '<' + tipo + ', ' + str(valor) + ', ' + str(linha) + '>'
-      print s    
-      #print '<' + repr(tipo) + ' ' + repr(valor) + ' ' + repr(linha) + '>'
+      print s
+      arq.write(s+'\n')      
+
+  f.close()
+  arq.close()
 
 if __name__ == "__main__":
   main()
