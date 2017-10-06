a129a1695816a2c3cd544e548f65a19e0e69bf57
MATHEUS SAPIA GUERRA <a1711610@debian9-dacom.dacom>
2017-10-05 21:01:27 -0300

    Implementando Syn

--- TppLex.py
+++ TppLex.py
@@ -4,117 +4,121 @@
 import ply.lex as lex
 import sys
 
-#hash de palavras reservadas
-reserved = {
-  'se'        :'SE',
-  'então'     :'ENTAO',
-  'senão'     :'SENAO',
-  'fim'       :'FIM',
-  'repita'    :'REPITA' ,
-  'flutuante' :'FLUTUANTE',
-  'retorna'   :'RETORNA',
-  'até'       :'ATE',
-  'leia'      :'LEIA',
-  'escreve'   :'ESCREVE',
-  'inteiro'   :'INTEIRO',
-  'principal' :'PRINCIPAL',
+class Lexer:
 
-}
-#vetor de tokens concatenado com o hash de palavras reservadas
-tokens = [
-  'ID',
-  'VIRGULA',
-  'ATRIBUICAO',
-  'COMENTARIO',  
+  def __init__(self):
+        self.lexer = lex.lex(debug=False, module=self, optimize=False)
 
-  'SOMA',
-  'SUBR',
-  'VEZES',
-  'DIVIDE',   
-  'IGUAL',
+  #hash de palavras reservadas
+  reserved = {
+    'se'        :'SE',
+    'então'     :'ENTAO',
+    'senão'     :'SENAO',
+    'fim'       :'FIM',
+    'repita'    :'REPITA' ,
+    'flutuante' :'FLUTUANTE',
+    'retorna'   :'RETORNA',
+    'até'       :'ATE',
+    'leia'      :'LEIA',
+    'escreve'   :'ESCREVE',
+    'inteiro'   :'INTEIRO',
+    'principal' :'PRINCIPAL',
 
-  'MENOR',
-  'MAIOR',
-  'MENORIGUAL',
-  'MAIORIGUAL',
-  'ELOGICO',
-  'OULOGICO',
-  'NEGACAO',
-  'DIFERENTE',
+  }
+  #vetor de tokens concatenado com o hash de palavras reservadas
+  tokens = [
+    'ID',
+    'VIRGULA',
+    'ATRIBUICAO',
+    'COMENTARIO',  
 
-  'ABREPAR',
-  'FECHAPAR',
-  'DOISPONTOS',
-  'ABRECOLCH',
-  'FECHACOLCH',   
-  'CHAVEDIR',
-  'CHAVEESQ',
+    'SOMA',
+    'SUBR',
+    'VEZES',
+    'DIVIDE', 
+      
+    'IGUAL',
+    'MENOR',
+    'MAIOR',
+    'MENORIGUAL',
+    'MAIORIGUAL',
+    'ELOGICO',
+    'OULOGICO',
+    'NEGACAO',
+    'DIFERENTE',
 
-] + list(reserved.values())
+    'ABREPAR',
+    'FECHAPAR',
+    'DOISPONTOS',
+    'ABRECOLCH',
+    'FECHACOLCH',   
+    'CHAVEDIR',
+    'CHAVEESQ',
 
-# Expressão regular para tokens simples
-t_SOMA = r'\+'
-t_SUBR = r'\-'
-t_VEZES = r'\*'
-t_DIVIDE = r'\/'
-t_IGUAL = r'\='
-t_VIRGULA = r'\,'
-t_ATRIBUICAO = r'\:\='
-t_MENOR = r'\<'
-t_MAIOR = r'\>'
-t_MENORIGUAL = r'\<\='
-t_MAIORIGUAL = r'\>\='
-t_ABREPAR  = r'\('
-t_FECHAPAR  = r'\)'
-t_DOISPONTOS = r'\:'
-t_ABRECOLCH = r'\['
-t_FECHACOLCH = r'\]'
-t_ELOGICO = r'\&\&'
-t_OULOGICO = r'\|\|'
-t_NEGACAO = r'\!'
-t_DIFERENTE = r'\!\='
+  ] + list(reserved.values())
+
+  # Expressão regular para tokens simples
+  t_SOMA = r'\+'
+  t_SUBR = r'\-'
+  t_VEZES = r'\*'
+  t_DIVIDE = r'\/'
+  t_IGUAL = r'\='
+  t_VIRGULA = r'\,'
+  t_ATRIBUICAO = r'\:\='
+  t_MENOR = r'\<'
+  t_MAIOR = r'\>'
+  t_MENORIGUAL = r'\<\='
+  t_MAIORIGUAL = r'\>\='
+  t_ABREPAR  = r'\('
+  t_FECHAPAR  = r'\)'
+  t_DOISPONTOS = r'\:'
+  t_ABRECOLCH = r'\['
+  t_FECHACOLCH = r'\]'
+  t_ELOGICO = r'\&\&'
+  t_OULOGICO = r'\|\|'
+  t_NEGACAO = r'\!'
+  t_DIFERENTE = r'\!\='
 
 
-# Expressão regular para identificadores e palavras reservadas
-def t_ID(t):
-    r'[a-zA-Z][a-zA-Z_0-9à-ú]*'
-    t.type = reserved.get(t.value,'ID') #verifica se é palavra reservada
+  # Expressão regular para identificadores e palavras reservadas
+  def t_ID(t):
+      r'[a-zA-Z][a-zA-Z_0-9à-ú]*'
+      t.type = reserved.get(t.value,'ID') #verifica se é palavra reservada
+      return t
+
+  # Expressão regular para comentários, com tratamento de contagem de linhas
+  def t_COMENTARIO(t):
+    r'\{[\s]*[^}]*[\s]*[^{]*\}'
+    for x in xrange(0,len(t.value)): #verificando a quantidade de quebra de linhas
+      if t.value[x] == '\n':
+        t.lexer.lineno += 1
     return t
 
-# Expressão regular para comentários, com tratamento de contagem de linhas
-def t_COMENTARIO(t):
-  r'\{[\s]*[^}]*[\s]*[^{]*\}'
-  for x in xrange(0,len(t.value)): #verificando a quantidade de quebra de linhas
-    if t.value[x] == '\n':
-      t.lexer.lineno += 1
-  return t
+  # Expressão regular para identificar numeros flutuante, considerando o ponto (ex: 2. é flutuante)
+  def t_FLUTUANTE(t):
+      r'[0-9]+[\.][0-9]*'
+      t.value = float(t.value)   
+      return t  
 
-# Expressão regular para identificar numeros flutuante, considerando o ponto (ex: 2. é flutuante)
-def t_FLUTUANTE(t):
-    r'[0-9]+[\.][0-9]*'
-    t.value = float(t.value)   
-    return t  
+  #exŕessão regular para identificar números inteiros
+  def t_INTEIRO(t):
+      r'[0-9]+'
+      t.value = int(t.value)   
+      return t
 
-#exŕessão regular para identificar números inteiros
-def t_INTEIRO(t):
-    r'[0-9]+'
-    t.value = int(t.value)   
-    return t
-
-# regra para contagem de linhas
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+  # regra para contagem de linhas
+  def t_newline(t):
+      r'\n+'
+      t.lexer.lineno += len(t.value)
 
 
-# caracteres ignorados
-t_ignore  = '\t '
+  # caracteres ignorados
+  t_ignore  = '\t '
 
-# Mostrar erro
-def t_error(t):
-    #print "Caractére ilegal" + t.value[0]
-    t.lexer.skip(1)
-
+  # Mostrar erro
+  def t_error(t):
+      #print "Caractére ilegal" + t.value[0]
+      t.lexer.skip(1)
 
 
 
--- TppSyn.py
+++ TppSyn.py
@@ -1,10 +1,77 @@
+# -*- coding: utf-8 -*-
+
 from ast import AST
 from ply import yacc
-from TppSyn import tokens
+from TppLex import Lexer
 
-def programa(t):
-	'''programa : lista_declaracoes '''
-	t[0] = t[1]
+class Tree:
+
+    def __init__(self, type_node, child=[], value=''):
+        self.type = type_node
+        self.child = child
+        self.value = value
+
+    def __str__(self):
+        return self.type
+
+class Parser:
+
+    def __init__(self, code):
+        lex = Lexer()
+        self.tokens = lex.tokens
+        self.precedence = (           
+            ('left', 'IGUAL', 'NEGACAO', 'MAIORIGUAL', 'MAIOR', 'MENORIGUAL', 'MENOR'),
+            ('left', 'SOMA', 'SURB'),
+            ('left', 'VEZES', 'DIVIDE'),
+        )
+        parser = yacc.yacc(debug=False, module=self, optimize=False)
+        self.ast = parser.parse(code)
+
+
+	def p_programa(self, p):
+		'''
+		programa : lista_declaracoes
+		'''
+		p[0] = Tree('programa',[p[1]])
+
 	
-def lista_declaracoes(t):
-	
+	def p_lista_declaracoes(self, p):
+		'''
+		lista_declaracoes : lista_declaracoes declaracao
+						  	|declaracao
+		'''
+		if (len(p) == 3):
+			p[0] = Tree('lista_declaracoes', [p[1],p[2]])
+		elif (len(p) == 2):
+			p[0] = Tree('lista_declaracoes', [p[1]])
+
+	def p_declaracao(self, p):
+		'''
+		declaracao: declaracao_variaveis
+					|inicializacao_variaveis
+					|declaracao_funcao
+
+		'''
+		p[0] = Tree('declaracao', [p[1]])
+
+	def p_declaracao_variaveis(self, p):
+		'''
+		declaracao_variaveis: tipo ":" lista_variaveis
+		'''
+		p[0] = Tree('declaracao_variaveis', [p[1],p[3]], p[2])
+
+	def p_inicializacao_variaveis(self, p):
+		'''
+		inicializacao_variaveis: atribuicao
+
+		'''
+		p[0] = Tree('inicializacao_variaveis', [p[1]])
+
+	def p_lista_variaveis():
+		'''
+		lista_variaveis: lista_variaveis "," var
+						 |var
+		'''
+		if
+
+		########################### socorrrrrooooo #################
